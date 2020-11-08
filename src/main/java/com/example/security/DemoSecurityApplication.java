47daeb9beb3caf9aafad606650c9a04b5830f55e
Muhammad Bagus Oka <mekari@MEKARIs-MacBook-Pro.local>
2020-11-08 11:37:45 +0700

    Init + Authentication

---
+++ src/main/java/com/example/security/DemoSecurityApplication.java
@@ -0,0 +1,69 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSecurityApplication.class, args);
+    }
+
+}
+
+@RestController
+class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Welcome</h1>";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "<h1>Welcome User</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "<h1>Welcome Admin</h1>";
+    }
+}
+
+@Configuration
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("blah")
+            .password("blah")
+            .roles("USER")
+            .and()
+            .withUser("foo")
+            .password("foo")
+            .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/**").hasRole("ADMIN")
+            .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
